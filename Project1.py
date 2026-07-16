3469f3cc26bcdf68be0af203d0f2f6619253fa29
Nikitha Kantha <nikithakantha@gmail.com>
2026-07-16 18:54:52 +0530

    Add changes

---
+++ Project1.py
@@ -0,0 +1,19 @@
+def calcu(num1 ,op ,num2):
+    if op == '+':
+        return num1 + num2
+    elif op == '-':
+        return num1 - num2
+    elif op == '*':
+        return num1 * num2
+    elif op == '/':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Error: Invalid operator"
+num1 = float(input("Enter the first number: "))
+op = input("Enter the operator (+, -, *, /): ")
+num2 = float(input("Enter the second number: "))
+result = calcu(num1, op, num2)
+print("Result: ",result)

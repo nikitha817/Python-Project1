f51ce4567d806f4269c97be78df670b1194cbdda
Nikitha Kantha <nikithakantha@gmail.com>
2026-07-24 14:28:43 +0530

    Add changes

--- BGM-Calculator/Project7.py
+++ BGM-Calculator/Project7.py
@@ -1,12 +1,23 @@
-def bgm_calcu(weight,height):
+def bmi_calcu(weight,height):
     BMI = weight / (height * height)
     return BMI
+def bmi_categ(BMI):
+    if BMI < 18.5:
+        print("Underweight")
+    elif BMI < 25:
+        print("Normal weight")
+    elif BMI < 30:
+        print("Overweight")
+    else:
+        print("Obese")
 def main():
     try:
-        weight = int(input("Enter weight: "))
-        height = int(input("Enter height: "))
+        weight = float(input("Enter weight: "))
+        height = float(input("Enter height: "))
     except ValueError:
         print("Please enter a integer")
-    bgm_calcu(weight,height)
+    BMI = bmi_calcu(weight,height)
+    print(f"BMI: {BMI}")
+    bmi_categ(BMI)
 if  __name__ == "__main__":
     main()

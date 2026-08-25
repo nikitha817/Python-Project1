4eb531f2d7e190029e12f1ee3a8469aadd11c8f9
Nikitha Kantha <nikithakantha@gmail.com>
2026-08-25 20:00:49 +0530

    Filtering using conditions

--- Pandas.py/Dataframe.py
+++ Pandas.py/Dataframe.py
@@ -1,12 +1,14 @@
 import pandas as pd
-df = pd.DataFrame({
+Data = {
     "Name":["Shiv", "Nove", "Git"],
     "Age":[23, 21, 20],
     "Course":["MPCS", "MAICS", "MSCS"]
-}, index=["Student1","Student2","Student3"])
+}
+df = pd.DataFrame(Data, index=["Student1", "Student2", "Student3"])
 df["Marks"] = [56, 76, 88]
 #new rows
 new_row = pd.DataFrame([{"Name":"Mari","Age":19,"Course":"BBA","Marks":78},
                        {"Name":"Heru","Age":21,"Course":"Bcom","Marks":88}],index=["Student4", "Student5"])
-df =pd.concat([df,new_row])
-print(df)
+df = pd.concat([df,new_row])
+df = df.sort_values(by=['Marks'],ascending=False)
+print(df.iloc[0:3])

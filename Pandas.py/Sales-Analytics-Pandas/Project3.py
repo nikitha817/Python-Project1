d581ff7294e1a6e49e374e213dcc24f3b03b4631
Nikitha Kantha <nikithakantha@gmail.com>
2026-09-01 21:46:41 +0530

    Category Analysis

--- Pandas.py/Sales-Analytics-Pandas/Project3.py
+++ Pandas.py/Sales-Analytics-Pandas/Project3.py
@@ -13,7 +13,7 @@ df["Revenue"] = df["Units_Sold"] * df["Unit_Price"]
 df["Cost"] = df["Units_Sold"] * df["Cost_Price"]
 df["Profit"] = df["Revenue"] - df["Cost"]
 print(df.head())
-
+"""
 #Total Revenue, Cost and Profit
 print("Total Revenue:", df["Revenue"].sum())
 print("Total Cost:", df["Cost"].sum())
@@ -25,3 +25,8 @@ print("Best Performing Product:", df.groupby("Product")["Profit"].sum().idxmax()
 print("Best Selling Product:", df.groupby("Product")["Units_Sold"].sum().idxmax())
 #Worst selling product
 print("Worst Selling Product:", df.groupby("Product")["Units_Sold"].sum().idxmin())
+"""
+#Categorical Analysis
+print("Revenue by Category:", df.groupby("Category")["Revenue"].sum())
+print("Profit by Category:", df.groupby("Category")["Profit"].sum())
+print("Region wise Revenue:", df.groupby("Region")["Revenue"].sum())

1550d92c0b9fa9168029d7b057cd11768c79092a
Nikitha Kantha <nikithakantha@gmail.com>
2026-09-01 20:59:35 +0530

    Adding Revenue, Cost and Profit Columns

--- Pandas.py/Sales-Analytics-Pandas/Project3.py
+++ Pandas.py/Sales-Analytics-Pandas/Project3.py
@@ -7,3 +7,21 @@ print(df.info())
 print(df.describe())
 df["Date"] = pd.to_datetime(df["Date"])
 print(df.info("Date"))
+
+#Adding new columns
+df["Revenue"] = df["Units_Sold"] * df["Unit_Price"]
+df["Cost"] = df["Units_Sold"] * df["Cost_Price"]
+df["Profit"] = df["Revenue"] - df["Cost"]
+print(df.head())
+
+#Total Revenue, Cost and Profit
+print("Total Revenue:", df["Revenue"].sum())
+print("Total Cost:", df["Cost"].sum())
+print("Total Profit:", df["Profit"].sum())
+
+#best Profitable product
+print("Best Performing Product:", df.groupby("Product")["Profit"].sum().idxmax())
+#best selling product
+print("Best Selling Product:", df.groupby("Product")["Units_Sold"].sum().idxmax())
+#Worst selling product
+print("Worst Selling Product:", df.groupby("Product")["Units_Sold"].sum().idxmin())

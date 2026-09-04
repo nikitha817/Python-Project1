26ec2fc1a2bc4e1c1753a8d159d012edc05bd44e
Nikitha Kantha <nikithakantha@gmail.com>
2026-09-04 17:50:22 +0530

    using merge merging product info to df

--- Pandas.py/Sales-Analytics-Pandas/Project3.py
+++ Pandas.py/Sales-Analytics-Pandas/Project3.py
@@ -52,15 +52,16 @@ df = df.sort_values(by="Revenue", ascending=False)
 df = df.sort_values(by="Profit", ascending=False)
 print(df.head())
 print(df.tail())
-"""
 
 # Boolean Filtering
 print(df[(df["Revenue"] > 100000) & (df["Profit"] > 20000)])
 # Query Method
 print(df.query("Revenue > 100000"))
+"""
 
 # Performance Analysis
 df["Performance"] = df["Profit"].apply(lambda x: "High" if x >= 30000 else "Medium" if x >= 15000 else "Low")
+"""
 print(df[["Product", "Profit", "Performance"]].head())
 print(df["Performance"].value_counts())
 print(df.groupby("Category").agg({
@@ -70,4 +71,35 @@ print(df.groupby("Category").agg({
 }))
 print("Most Revenue-Generating, Profitable, and High-Volume Category:", df.groupby("Category").agg({"Revenue": "sum", "Profit": "sum", "Units_Sold": "sum"}).idxmax())
 print("Least Revenue-Generating, Profitable, and High-Volume Category:", df.groupby("Category").agg({"Revenue": "sum", "Profit": "sum", "Units_Sold": "sum"}).idxmin())
+"""
 
+
+# Product DataFrame
+product_info = pd.DataFrame({
+    "Product": ["Laptop", "Headphones", "Office Chair", "Desk", "Monitor",
+                "Keyboard", "Notebook", "Printer", "Pen Pack", "Bookshelf",
+                "Mouse", "Whiteboard", "Tablet", "Cabinet", "Calculator",
+                "Projector", "Study Table", "Markers", "USB Drive", "Lamp"],
+    
+    "Supplier": ["Dell", "Sony", "Ikea", "Ikea", "Samsung",
+                 "Logitech", "Classmate", "HP", "Cello", "Ikea",
+                 "Logitech", "Amazon Basics", "Samsung", "Godrej", "Casio",
+                 "Epson", "Ikea", "Camlin", "SanDisk", "Philips"],
+    
+    "Rating": [4.5, 4.3, 4.2, 4.1, 4.4,
+               4.6, 4.0, 4.2, 4.1, 4.3,
+               4.5, 4.0, 4.6, 4.2, 4.7,
+               4.1, 4.4, 4.0, 4.5, 4.2]
+})
+df = pd.merge(df, product_info, on="Product", how="left")
+#highest average rated product
+print("Highest Average Rated Product:", df.groupby("Supplier")["Rating"].mean().idxmax())
+print("Lowest Average Rated Product:", df.groupby("Supplier")["Rating"].mean().idxmin())
+print("Highest Revenue Supplier:", df.groupby("Supplier")["Revenue"].sum().idxmax())
+print("Lowest Revenue Supplier:", df.groupby("Supplier")["Revenue"].sum().idxmin())
+print("Total Revenue for each Supplier within each Category:", df.groupby(["Supplier", "Category"])["Revenue"].sum())
+print("Highest average product rating for each category:", df.groupby("Category")["Rating"].mean().idxmax())
+pivot_table3 = pd.pivot_table(df, index="Region", values=["Revenue", "Profit"], aggfunc="sum")
+print(pivot_table3)
+pivot_table4 = pd.pivot_table(df, index="Region", values="Profit", columns="Category", aggfunc="sum")
+print(pivot_table4)
